a4d6cfde94cd69a296920eaad0a89228f1ac2ef8
staskochkin <stkchkin@gmail.com>
2017-04-06 15:25:44 +0300

    Bump version to 1.3.2

--- src/MRAID/MRAIDSettings.m
+++ src/MRAID/MRAIDSettings.m
@@ -8,7 +8,7 @@
 
 #import "MRAIDSettings.h"
 
-NSString* const kMRAIDKitVersion    = @"1.3.1";
+NSString* const kMRAIDKitVersion    = @"1.3.2";
 BOOL SK_ENABLE_JS_LOG                  = NO;
 BOOL SK_SUPPRESS_JS_ALERT              = YES;
 BOOL SK_SUPPRESS_BANNER_AUTO_REDIRECT  = YES;
